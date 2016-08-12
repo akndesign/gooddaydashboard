3c011aaa79256eb2dc3c9e8fd08ce8a54ac5cc40
ALEXANDER <ALEXANDERKOZIOL@HOTMAIL.COM>
2016-08-12 02:51:11 +0100

    All lines are live, in the console and in the UI. Flash of startup doom, however.

--- javascript_tube.js
+++ javascript_tube.js
@@ -13,35 +13,53 @@ app.getUnderground = function(){
         url: weatherUrl + app_id + apiKey,
         method: "GET",
         success: function(response){
-            app.tubeService(response);
-            for (var i=0; i < response.length; i++) {
-                var serviceName = response[i].name;
-                var serviceStatus = response[i].lineStatuses[0].statusSeverityDescription;
-
-                if (serviceStatus !== 'Good Service'){
-                    console.log('Interruption');
-                }
-               
-                console.log(serviceName + " line is " + serviceStatus);
-
-            $('li > div').text(serviceStatus);
-
-            }
+            app.undergroundService(response);
+            //app.undergroundLine(response);
         }
     });
-
-}; 
-
-//$('#js-tfl' + nameofLine );
-
-app.tubeService = function(services){
-    console.log(services);
-     for (var i=0; i < services.length; i++) {
-                var serviceName = services[i].name;
-                var serviceStatus = services[i].lineStatuses[0].statusSeverityDescription;
-            }
 };
 
+/*app.undergroundLine = function(name){
+    for (var i=0; i < name.length; i++) {
+            var serviceName = name[i].name;
+            console.log(serviceName);
+           //$('#status').text(serviceName); 
+       }
+};*/
+
+app.undergroundService = function(name, services){
+            
+            for (var i=0; i < name.length; i++) {
+                var serviceID = name[i].id;
+                var serviceName = name[i].name;
+                var serviceStatus = name[i].lineStatuses[0].reason;
+
+                var changeTubeID = $('#tfl').attr('id', "js-tfl-" + serviceID);
+                var task = '<div class="status">' + serviceStatus + '</div>';
+                var Name = '<h3>' + serviceName + '</h3>';
+                
+                $('#lines').append(name);
+                $(changeTubeID).append(task);
+
+                //'<li>'+ serviceName + '</li>';
+                //var task = '<li>' + serviceStatus + '</li>'
+
+                /*var serviceStatus = name[i].lineStatuses[0].statusSeverityDescription;
+
+                if (serviceStatus === 'Good Service'){
+                    $('#js-tfl' + serviceName).append(serviceStatus);
+                    console.log(serviceStatus);
+                }
+                
+                if (serviceStatus !== 'Good Service'){
+                    console.log('Interruption' + serviceStatus);
+                    //$('#js-tfl'+ serviceName).append(serviceStatus);
+                }*/
+            }
+            //$('#js-tfl' + nameofLine );
+            //console.log(serviceName);
+            //$('#status').text(serviceName); 
+};
 
 app.timeofDay = function(){
     var currentTime = new Date();
@@ -55,54 +73,3 @@ app.init = function() {
 };
 
 $(document).ready(app.init);
-
-/*var app = {};
-
-app.getUnderground = function(){
-    var weatherUrl = 'https://api.tfl.gov.uk/line/mode/tube/status?';
-    var app_id = 'app_id=2a49b2c6';
-    var apiKey = '&app_key=19d97bbedc6a5b79a885b824afc220c3';
-
-    $.ajax({
-        url: weatherUrl + app_id + apiKey,
-        method: "GET",
-        success: function(response){
-            for (var i=0; i < response.length; i++) {
-                var serviceName = response[i].name;
-                var serviceStatus = response[i].lineStatuses[0].statusSeverityDescription;
-                
-             if (serviceStatus === 'Service Good') { 
-                $("#li").text(serviceStatus);
-                } //make an exception to W&C line, which opens later
-            if (serviceStatus === 'Service Closed') { 
-                $("#li").text(serviceStatus);
-            }console.log(serviceName + " line is " + serviceStatus);
-
-
-            //else if (serviceName === 'Bakerloo') //serviceName = id { 
-               // $("#bakerloo").text(serviceStatus);
-              //  }     
-            
-
-            //var serviceStatus = response[i].lineStatuses[0].statusSeverityDescription;
-            /*for (var key in response[0].lineStatuses[0]){
-                if (key === "statusSeverityDescription"){
-                    $("#bakerloo").text(serviceStatus);
-                }
-            /*for (i = 0; i < response.length; i++){
-                if (key === "description"){
-                    $("#status").text(response[0].description);
-                }
-             /*for (var key in response[0]){
-                if (key === "description"){
-                    $("#victoria").text(response[0].description);
-                }
-                    //$("#location").text(response.list[0][key]); for a future concept idea
-                /*} else if (key === "main") {
-                    var temperature = Math.round(response.list[1].main.temp);
-                    $("#central").text(temperature + "°C");
-                } else if (key === "weather") {
-                    var weatherCondition = response.list[1].weather[0].main;
-                    $("#circle").text(weatherCondition);
-                }*/  
-         
