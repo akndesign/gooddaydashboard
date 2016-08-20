a75d0e769d101c517f24394e226623e7ea97a330
ALEXANDER <ALEXANDERKOZIOL@HOTMAIL.COM>
2016-08-20 18:14:28 +0100

    commit prior to major refactoring changes

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ app.getWeather = function() {
     $.ajax({
         url: weatherUrl + city + celcius + apiKey,
         method: 'GET',
+        beforeSend: function(){ 
+        $('#weather').html('Loading...');
+        },
         success: function(response) {
             for (var key in response.list[0]) {
                 if (key === 'name') {
@@ -31,26 +34,29 @@ app.getWeather = function() {
 
 app.getUndergroundData = function() {
 
-    /*  var tflUrl = 'https://api.tfl.gov.uk/line/mode/tube/status?';
+    var tflUrl = 'https://api.tfl.gov.uk/line/mode/tube/status?';
     var app_id = 'app_id=2a49b2c6';
     var apiKey = '&app_key=19d97bbedc6a5b79a885b824afc220c3';
 
     $.ajax({
         url: tflUrl + app_id + apiKey,
         method: 'GET',
+        beforeSend: function(){ 
+        $('#service-board').html('Loading...');
+        },
         success: function(response){
             app.displayUndergroundService(response);
             app.undergroundNotifications(response);
         }
     });
-};*/
+};
 
-    //Dummy Data JSON Request
+  /*  //Dummy Data JSON Request
     $.getJSON('js/dummy-json/serviceclosed.json', function(response) {
         app.displayUndergroundService(response);
         app.undergroundNotifications(response);
     });
-};
+};*/
 
 app.displayUndergroundService = function(name) {
 
@@ -146,6 +152,9 @@ app.getAsteriods = function() {
     $.ajax({
         url: asteriodUrl + apiKey,
         method: 'GET',
+        beforeSend: function(){ 
+        $('#asteriod').html('Loading...');
+        },
         success: function(response) {
 
             moment.tz.add(['America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0']);
@@ -167,6 +176,116 @@ app.getAsteriods = function() {
     });
 };
 
+app.getGoogleCalendar = function() {
+
+    /*$.ajax({
+        url: 'https://www.googleapis.com/auth/calendar.readonly' + "559328610579-le432q0suco7hf1l4mohephcsvss0amm.apps.googleusercontent.com",
+        method: "GET",
+        dataType: 'json',
+        success: function(response) { 
+            checkAuth(response); 
+            handleAuthResult(response); 
+            handleAuthClick(response);
+        }
+    });
+      /**
+       * Check if current user has authorized this application.
+       */
+      function checkAuth() {
+        console.log(hello);
+        gapi.auth.authorize(
+          {
+            'client_id': "559328610579-le432q0suco7hf1l4mohephcsvss0amm.apps.googleusercontent.com",
+            'scope': ["https://www.googleapis.com/auth/calendar.readonly"].join(' '),
+            'immediate': true
+          }, handleAuthResult);
+      }
+
+      /**
+       * Handle response from authorization server.
+       *
+       * @param {Object} authResult Authorization result.
+       */
+      function handleAuthResult(authResult) {
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+          // Hide auth UI, then load client library.
+          authorizeDiv.style.display = 'none';
+          loadCalendarApi();
+        } else {
+          // Show auth UI, allowing the user to initiate authorization by
+          // clicking authorize button.
+          authorizeDiv.style.display = 'inline';
+        }
+      }
+
+      /**
+       * Initiate auth flow in response to user clicking authorize button.
+       *
+       * @param {Event} event Button click event.
+       */
+      function handleAuthClick(event) {
+        gapi.auth.authorize(
+          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+          handleAuthResult);
+        return false;
+      }
+
+      /**
+       * Load Google Calendar client library. List upcoming events
+       * once client library is loaded.
+       */
+      function loadCalendarApi() {
+        gapi.client.load('calendar', 'v3', listUpcomingEvents);
+      }
+
+      /**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+      function listUpcomingEvents() {
+        var request = gapi.client.calendar.events.list({
+          'calendarId': 'primary',
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 10,
+          'orderBy': 'startTime'
+        });
+
+        request.execute(function(resp) {
+          var events = resp.items;
+          appendPre('Upcoming events:');
+
+          if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+              var event = events[i];
+              var when = event.start.dateTime;
+              if (!when) {
+                when = event.start.date;
+              }
+              appendPre(event.summary + ' (' + when + ')')
+            }
+          } else {
+            appendPre('No upcoming events found.');
+          }
+
+        });
+      }
+
+      /**
+       * Append a pre element to the body containing the given message
+       * as its text node.
+       *
+       * @param {string} message Text to be placed in pre element.
+       */
+      function appendPre(message) {
+        var pre = document.getElementById('output');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+      }
+};
 
 //var asteriod = asteriodResponse + nasaAPIDay;
 
@@ -177,6 +296,7 @@ app.getAsteriods = function() {
 }*/
 
 app.init = function() {
+    app.getGoogleCalendar();
     app.getUndergroundData();
     app.displayUndergroundService(name);
     app.undergroundNotificationsOverlay();
