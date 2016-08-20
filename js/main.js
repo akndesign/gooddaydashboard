e5e3775ad64744eaff0042eff8d95423bb4a33ac
ALEXANDER <ALEXANDERKOZIOL@HOTMAIL.COM>
2016-08-20 23:10:03 +0100

    SERVICE CONDITIONS WORK!! AND ALSO ACCOMMODATE FOR THE NIGHT TUBE

--- js/main.js
+++ js/main.js
@@ -2,73 +2,102 @@ var app = {};
 
 //WEATHER APP
 
-app.getWeather = function() {
+app.getAJAX = function() {
+
     var weatherUrl = 'http://api.openweathermap.org/data/2.5/find?q=';
     var city = 'london';
     var celcius = '&units=metric';
-    var apiKey = '&appid=ea43d349fe09f49a0d21b5607b77208c';
-
-    $.ajax({
-        url: weatherUrl + city + celcius + apiKey,
-        method: 'GET',
-        beforeSend: function(){ 
-        $('#weather').html('Loading...');
-        },
-        success: function(response) {
-            for (var key in response.list[0]) {
-                if (key === 'name') {
-                    $('#city').text(response.list[0][key]);
-                } else if (key === 'main') {
-                    var temperature = Math.round(response.list[1].main.temp);
-                    $('#temperature').text(temperature + '°C');
-                } else if (key === 'weather') {
-                    var weatherCondition = response.list[1].weather[0].main;
-                    $('#weathercondition').text(weatherCondition + ', ');
-                }
-            }
-        }
-    });
-};
-
-//UNDERGROUND
-
-app.getUndergroundData = function() {
+    var apiWeatherKey = '&appid=ea43d349fe09f49a0d21b5607b77208c';
 
     var tflUrl = 'https://api.tfl.gov.uk/line/mode/tube/status?';
     var app_id = 'app_id=2a49b2c6';
-    var apiKey = '&app_key=19d97bbedc6a5b79a885b824afc220c3';
+    var apiUndergroundKey = '&app_key=19d97bbedc6a5b79a885b824afc220c3';
+
+    var asteriodUrl = 'https://api.nasa.gov/neo/rest/v1/feed/today?detailed=false';
+    var apiKey = '&api_key=PT3Ux5XFGFqnK869ovrGVMS5SBciZGmQ0I0LnkrC';
+
 
     $.ajax({
-        url: tflUrl + app_id + apiKey,
+        url: weatherUrl + city + celcius + apiWeatherKey,
         method: 'GET',
-        beforeSend: function(){ 
-        $('#service-board').html('Loading...');
+        beforeSend: function() {
+            $('#weather').html('Loading...');
         },
-        success: function(response){
-            app.displayUndergroundService(response);
-            app.undergroundNotifications(response);
+        success: function(weatherResponse) {
+
+            app.displayWeather(weatherResponse);
+        }
+    });
+
+    /*$.ajax({
+        url: tflUrl + app_id + apiUndergroundKey,
+        method: 'GET',
+        beforeSend: function() {
+            $('#service-board').html('Loading...');
+        },
+        success: function(undergroundResponse) {
+
+            app.displayUndergroundService(undergroundResponse);
+            app.undergroundOverlay(undergroundResponse);
+            app.displayUndergroundOverlay(undergroundResponse);
+        }
+    });
+
+    //Dummy Underground JSON Request */
+    $.getJSON('js/dummy-json/mixedservice.json', function(undergroundResponse) {
+        app.displayUndergroundService(undergroundResponse);
+        app.undergroundOverlay(undergroundResponse);
+        app.displayUndergroundOverlay(undergroundResponse);
+        
+        });
+
+    $.ajax({
+        url: asteriodUrl + apiKey,
+        method: 'GET',
+        beforeSend: function() {
+            $('#asteriod').html('Loading...');
+        },
+        success: function(asteriodResponse) {
+
+            app.displayAsteriods(asteriodResponse);
         }
     });
 };
 
-  /*  //Dummy Data JSON Request
-    $.getJSON('js/dummy-json/serviceclosed.json', function(response) {
-        app.displayUndergroundService(response);
-        app.undergroundNotifications(response);
-    });
-};*/
+//Display Weather 
 
-app.displayUndergroundService = function(name) {
+app.displayWeather = function(weatherResponse) {
 
-    if (!name) {
-        name = [];
+    if (!weatherResponse) {
+        weatherResponse = [];
     }
 
-    for (var i = 0; i < name.length; i++) {
+    for (var key in weatherResponse.list[0]) {
+        if (key === 'name') {
+            $('#city').text(weatherResponse.list[0][key]);
+        } else if (key === 'main') {
+            var temperature = Math.round(weatherResponse.list[1].main.temp);
+            $('#temperature').text(temperature + '°C');
+        } else if (key === 'weather') {
+            var weatherCondition = weatherResponse.list[1].weather[0].main;
+            $('#weathercondition').text(weatherCondition + ', ');
+        }
+    }
+};
 
-        var undergroundID = name[i].id;
-        var undergroundName = name[i].name;
-        var undergroundStatus = name[i].lineStatuses[0].statusSeverityDescription;
+//Display Underground, Per-Line
+
+app.displayUndergroundService = function(undergroundResponse) {
+
+    if (!undergroundResponse) {
+        undergroundResponse = [];
+    }
+
+    for (var i = 0; i < undergroundResponse.length; i++) {
+
+        var undergroundID = undergroundResponse[i].id;
+        var undergroundName = undergroundResponse[i].name;
+        var undergroundStatus = undergroundResponse[i].lineStatuses[0].statusSeverityDescription;
 
         var addLineID = $('#tfl').attr('id', undergroundID);
         var addLine = '<h6>' + undergroundName + ' ' + undergroundStatus + '</h6>';
@@ -76,32 +105,175 @@ app.displayUndergroundService = function(name) {
     }
 };
 
-//RESPONSES
+//Hide Underground Overlay, on Hover
 
-app.undergroundNotifications = function(service) {
+app.undergroundOverlay = function(undergroundResponse) {
 
+    $('.toggle').hover(function() {
+        $('#service-notifications').fadeOut('fast');
+    }, function() {
+        $('#service-notifications').fadeIn('fast');
+    });
+};
+
+//Display Underground Overlay
+
+app.displayUndergroundOverlay = function(undergroundResponse) {
+
+    var goodService = [];
     var serviceClosed = [];
+    var interruption = [];
 
-    if (!service) {
-        service = [];
-    }
+    var otherLines = [' other lines'];
+    var pluralLines = [' lines'];
+    var singleLine = [' line'];
 
-    for (var i = 0; i < service.length; i++) {
+    undergroundResponse.forEach(function(line) {
+        
+        var undergroundName = line.name;
+        var description = line.lineStatuses[0].statusSeverityDescription;
 
-        var undergroundName;
         //var linePural = service.length(count >= 1 ? this: 's');
 
-        if (i === service.length - 2) {
-            undergroundName = service[i].name + ' and ';
-        } else if (i === service.length - 1) {
-            undergroundName = service[i].name + ' lines';
+       /* if (i === undergroundResponse.length - 2) {
+            undergroundName = undergroundResponse[i].name + ' and ';
+        } else if (i === undergroundResponse.length - 1) {
+            undergroundName = undergroundResponse[i].name + ' lines';
         } else {
-            undergroundName = service[i].name + ', ';
+            undergroundName = undergroundResponse[i].name + ', ';
         }
         //else if (i === service.length - 4) {
         //undergroundName = service[i].name + ' and other ';
 
-        var undergroundStatus = service[i].lineStatuses[0].statusSeverityDescription;
+
+    */
+        switch (description) {
+
+            case 'Good Service':
+                
+                $('#good-service').text('Good').append("<div id='good-title'></div>");
+                $('#good-title').text('service');
+                break;
+            
+            case 'Service Closed':
+                
+                serviceClosed.push(undergroundName);
+
+                if ($(serviceClosed).length === 1) {
+
+                    var serviceClosedSingle = serviceClosed.concat(singleLine);
+
+                    $('#good-service').addClass('is-hidden');
+                    $('#service-closed').text('Service Closed on the ');
+                    //$('#service-closed').append(undergroundName);
+                    $('#service-closed').append(serviceClosedSingle);
+                    console.log(serviceClosedSingle);
+                    //$(serviceClosedString.push('and'));
+
+                } else if ($(serviceClosed).length === 2) {
+
+                    var serviceClosedPlural = serviceClosed.join(', ').concat(pluralLines);
+
+                    $('#good-service').addClass('is-hidden');
+                    $('#service-closed').text('Service Closed on the ');
+                    //$('#service-closed').append(undergroundName);
+                    $('#service-closed').append(serviceClosedPlural);
+                    //$(serviceClosedString.push('and'));
+
+                } else {
+
+                    var today = new Date();
+                    
+                    if (today.getDay() == 6 || today.getDay() == 0) {
+                            $('#good-service').addClass('is-hidden');
+                            $('#service-closed').text('Night Tube Available');
+                        } else { $('#good-service').addClass('is-hidden');
+                                $('#service-closed').text('Service Closed');
+                    }
+
+                }
+
+                break;
+            
+            default:
+
+                /*interruption.push(undergroundName);
+
+                
+
+                if ($(serviceClosed).length === 1) {
+
+                    var serviceClosedSingle = serviceClosed.concat(singleLine);
+
+                    $('#good-service').addClass('is-hidden');
+                    $('#service-closed').text('Service Closed on the ');
+                    //$('#service-closed').append(undergroundName);
+                    $('#service-closed').append(serviceClosedSingle);
+                    console.log(serviceClosedSingle);
+                    //$(serviceClosedString.push('and'));
+
+                if ($(interruption).length >= 4){
+                    $('#good-service').addClass('is-hidden');
+                    $('#interruptions').text('Interruptions on the ');
+                    $('#interruptions').append(interruptionString);
+                    var interruptionString = interruption.join(', ').concat(pluralLines);
+                    //$('#interruptions').text('Many Interruptions');
+                    //$(interruption.push("lines"));
+                    otherLines
+
+                } else {
+                    console.log(interruption);
+                    $('#good-service').addClass('is-hidden');
+                    $('#interruptions').text('Interruptions on the ');
+                    //$('#service-closed').append(undergroundName);
+                    $('#interruptions').append(interruptionString);
+                }
+
+                /*if ($(interruption).length === 1){
+                    serviceClosed.push(undergroundName);
+                    undergroundName = [line.name];
+                    $('.title').text('Interruptions on the ');
+                    $('#good-service').addClass('is-hidden');
+                    $('#interruptions').append(undergroundName);
+
+                } else if ($(interruption).length <= 3){
+                    console.log(interruption);
+                    undergroundName = [line.name] + ', ';
+                    $('.title').text('Interruptions on the ');
+                    $('#good-service').addClass('is-hidden');
+                    $('#interruptions').append(undergroundName);
+                } else {
+                    console.log(interruption);
+                    $('.title').text('Interruptions on many lines');
+                    $('#good-service').addClass('is-hidden');
+                    //$('#interruptions').append(undergroundName);
+                }*/ break;
+        }
+    });
+
+
+    /*var serviceClosed = [];
+
+    if (!undergroundResponse) {
+        undergroundResponse = [];
+    }
+
+    for (var i = 0; i < undergroundResponse.length; i++) {
+
+        var undergroundName;
+        //var linePural = service.length(count >= 1 ? this: 's');
+
+        if (i === undergroundResponse.length - 2) {
+            undergroundName = undergroundResponse[i].name + ' and ';
+        } else if (i === undergroundResponse.length - 1) {
+            undergroundName = undergroundResponse[i].name + ' lines';
+        } else {
+            undergroundName = undergroundResponse[i].name + ', ';
+        }
+        //else if (i === service.length - 4) {
+        //undergroundName = service[i].name + ' and other ';
+
+        var undergroundStatus = undergroundResponse[i].lineStatuses[0].statusSeverityDescription;
 
         if (undergroundStatus === 'Good Service') {
             $('#good-service').text('Good').append("<div id='good-title'></div>");
@@ -113,7 +285,7 @@ app.undergroundNotifications = function(service) {
 
             if (i === service.length - 2) {
                 $('#service-closed').text('Service Closed').append(undergroundName);
-                
+
             }
             return ($('#service-closed').text('Service Closed'));
 
@@ -125,16 +297,7 @@ app.undergroundNotifications = function(service) {
 
         }
         //$('#interruptions').addClass('is-hidden');
-    }
-};
-
-app.undergroundNotificationsOverlay = function(service) {
-
-    $('.toggle').hover(function() {
-        $('#service-notifications').fadeOut('fast');
-    }, function() {
-        $('#service-notifications').fadeIn('fast');
-    });
+    }*/
 };
 
 /*var count = 0;
@@ -144,165 +307,35 @@ app.undergroundNotificationsOverlay = function(service) {
         
     };*/
 
-app.getAsteriods = function() {
+//Display Asteriods
 
-    var asteriodUrl = 'https://api.nasa.gov/neo/rest/v1/feed/today?detailed=false';
-    var apiKey = '&api_key=PT3Ux5XFGFqnK869ovrGVMS5SBciZGmQ0I0LnkrC';
+app.displayAsteriods = function(asteriodResponse) {
 
-    $.ajax({
-        url: asteriodUrl + apiKey,
-        method: 'GET',
-        beforeSend: function(){ 
-        $('#asteriod').html('Loading...');
-        },
-        success: function(response) {
+    moment.tz.add(['America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0']);
 
-            moment.tz.add(['America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0']);
+    var nasaAPIDay = moment().tz('America/Los_Angeles').format('YYYY-MM-DD');
+    var asteriod = asteriodResponse.near_earth_objects[nasaAPIDay];
 
-            var nasaAPIDay = moment().tz('America/Los_Angeles').format('YYYY-MM-DD');
-            var asteriod = response.near_earth_objects[nasaAPIDay];
+    for (var i = 0; i < asteriod.length; i++) {
+        var asteriodData = asteriod[i].is_potentially_hazardous_asteroid;
 
-            for (var i = 0; i < asteriod.length; i++) {
-                var asteriodData = asteriod[i].is_potentially_hazardous_asteroid;
-
-                if (asteriodData) {
-                    return $('#asteriod').text('Asteroids are nearby');
-                } else {
-                    $('#asteriod').text('No nearby asteriods');
-                }
-            }
-
-        }
-    });
-};
-
-app.getGoogleCalendar = function() {
-
-    /*$.ajax({
-        url: 'https://www.googleapis.com/auth/calendar.readonly' + "559328610579-le432q0suco7hf1l4mohephcsvss0amm.apps.googleusercontent.com",
-        method: "GET",
-        dataType: 'json',
-        success: function(response) { 
-            checkAuth(response); 
-            handleAuthResult(response); 
-            handleAuthClick(response);
-        }
-    });
-      /**
-       * Check if current user has authorized this application.
-       */
-      function checkAuth() {
-        console.log(hello);
-        gapi.auth.authorize(
-          {
-            'client_id': "559328610579-le432q0suco7hf1l4mohephcsvss0amm.apps.googleusercontent.com",
-            'scope': ["https://www.googleapis.com/auth/calendar.readonly"].join(' '),
-            'immediate': true
-          }, handleAuthResult);
-      }
-
-      /**
-       * Handle response from authorization server.
-       *
-       * @param {Object} authResult Authorization result.
-       */
-      function handleAuthResult(authResult) {
-        var authorizeDiv = document.getElementById('authorize-div');
-        if (authResult && !authResult.error) {
-          // Hide auth UI, then load client library.
-          authorizeDiv.style.display = 'none';
-          loadCalendarApi();
+        if (asteriodData) {
+            return $('#asteriod').text('Asteroids are nearby');
         } else {
-          // Show auth UI, allowing the user to initiate authorization by
-          // clicking authorize button.
-          authorizeDiv.style.display = 'inline';
+            $('#asteriod').text('No nearby asteriods');
         }
-      }
-
-      /**
-       * Initiate auth flow in response to user clicking authorize button.
-       *
-       * @param {Event} event Button click event.
-       */
-      function handleAuthClick(event) {
-        gapi.auth.authorize(
-          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-          handleAuthResult);
-        return false;
-      }
-
-      /**
-       * Load Google Calendar client library. List upcoming events
-       * once client library is loaded.
-       */
-      function loadCalendarApi() {
-        gapi.client.load('calendar', 'v3', listUpcomingEvents);
-      }
-
-      /**
-       * Print the summary and start datetime/date of the next ten events in
-       * the authorized user's calendar. If no events are found an
-       * appropriate message is printed.
-       */
-      function listUpcomingEvents() {
-        var request = gapi.client.calendar.events.list({
-          'calendarId': 'primary',
-          'timeMin': (new Date()).toISOString(),
-          'showDeleted': false,
-          'singleEvents': true,
-          'maxResults': 10,
-          'orderBy': 'startTime'
-        });
-
-        request.execute(function(resp) {
-          var events = resp.items;
-          appendPre('Upcoming events:');
-
-          if (events.length > 0) {
-            for (i = 0; i < events.length; i++) {
-              var event = events[i];
-              var when = event.start.dateTime;
-              if (!when) {
-                when = event.start.date;
-              }
-              appendPre(event.summary + ' (' + when + ')')
-            }
-          } else {
-            appendPre('No upcoming events found.');
-          }
-
-        });
-      }
-
-      /**
-       * Append a pre element to the body containing the given message
-       * as its text node.
-       *
-       * @param {string} message Text to be placed in pre element.
-       */
-      function appendPre(message) {
-        var pre = document.getElementById('output');
-        var textContent = document.createTextNode(message + '\n');
-        pre.appendChild(textContent);
-      }
+    }
 };
 
-//var asteriod = asteriodResponse + nasaAPIDay;
-
-//console.log(asteriod);
-
-/*for (var key in asteriod) {
-    console.log(response);
-}*/
 
 app.init = function() {
+    app.getAJAX();
+    app.displayWeather();
+    app.displayUndergroundService();
+    app.undergroundOverlay();
+    app.displayUndergroundOverlay();
+    app.displayAsteriods();
     app.getGoogleCalendar();
-    app.getUndergroundData();
-    app.displayUndergroundService(name);
-    app.undergroundNotificationsOverlay();
-    // app.undergroundNotifications();
-    app.getWeather();
-    app.getAsteriods();
 };
 
 $(document).ready(app.init);
