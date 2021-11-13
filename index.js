f2a8ad54170eac50498a53e74cef0421209dac59
akndesign <alexanderkoziol@hotmail.com>
2021-11-13 01:14:45 -0500

    commiy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone'); // dependent on utc plugin
 
-app.use(express.static(path.join(__dirname, '/public'))); 
+//app.use(express.static(path.join(__dirname, '/public'))); 
 //app.use(express.static(path.join(__dirname + '/')));
 
 app.use((req, res, next) => {
@@ -33,6 +33,17 @@ app.get('/nasa-asteriods/', (req, res, next) => {
     }
   )
 });
+
+const cool = require('cool-ascii-faces');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 /*const server = http.createServer(app);
 const port = 3000;
