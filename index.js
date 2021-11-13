664c5fdf2683b7e8e231638ac5a4257f9b2bc9b3
akndesign <alexanderkoziol@hotmail.com>
2021-11-13 14:46:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ const timezone = require('dayjs/plugin/timezone'); // dependent on utc plugin
 app.use(express.static(path.join(__dirname, '/public'))); 
 //app.use(express.static(path.join(__dirname + '/')));
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-/*const asteroidUrl = 'https://ssd-api.jpl.nasa.gov/fireball.api?date-min=';
+const asteroidUrl = 'https://ssd-api.jpl.nasa.gov/fireball.api?date-min=';
 const nasaAPIDay = new Date().toLocaleString({timeZone: "America/Los_Angeles"}).split("T")[0];
 const nasaAPIURL = (asteroidUrl + nasaAPIDay); 
 
-console.log(nasaAPIDay, nasaAPIURL);*/
+console.log(nasaAPIDay, nasaAPIURL);
 
 app.get('/nasa-asteriods/', (req, res, next) => {
   request(
@@ -32,7 +32,7 @@ app.get('/nasa-asteriods/', (req, res, next) => {
       res.json(JSON.parse(body));
     }
   )
-});
+});*/
 
 //const cool = require('cool-ascii-faces');
 const PORT = process.env.PORT || 5000;
