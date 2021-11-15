90bf05b2c83ba42c1fd4a73a4c4622cb6ae20a51
akndesign <alexanderkoziol@hotmail.com>
2021-11-15 02:12:18 -0500

    Updating image locations

--- index.js
+++ index.js
@@ -27,61 +27,12 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname + "/public"));
 
-
-
-
-/*app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.use(express.static(__dirname + "/public"));
-
 app.use(express.static(__dirname + "/public/img"));
-*/
+
 app.listen(process.env.PORT || 5000, function () {
     console.log('Node app is working!');
 });
 
-
-
-
-
-/*const express = require('express');
-const request = require('request');
-const app = express();
-const http = require('http');
-const path = require('path');
-const dayjs = require('dayjs');
-const utc = require('dayjs/plugin/utc');
-const timezone = require('dayjs/plugin/timezone'); // dependent on utc plugin
-
-
-app.use(express.static(path.join(__dirname, 'index.html'))); 
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + 'index.html');
-});
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
-//const asteroidUrl = 'https://ssd-api.jpl.nasa.gov/fireball.api?date-min=';
-//const nasaAPIDay = new Date().toLocaleString({timeZone: "America/Los_Angeles"}).split("T")[0];
-//const nasaAPIURL = (asteroidUrl + nasaAPIDay); 
-
-//console.log(nasaAPIDay, nasaAPIURL);
-
-
-
-const PORT = process.env.PORT || 5000;
-express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-/*const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);*/
 
 
 
