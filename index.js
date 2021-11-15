ecc7ff4114ac9c78f8a700efd44d26ac6dd4d746
akndesign <alexanderkoziol@hotmail.com>
2021-11-14 22:36:36 -0500

    minor tweaks to index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone'); // dependent on utc plugin
 
-app.use(express.static(path.join(__dirname, '/public'))); 
-//app.use(express.static(path.join(__dirname + '/')));
+//app.use(express.static(path.join(__dirname, '/public'))); 
+app.use(express.static(path.join(__dirname + '/')));
 
 /*app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
