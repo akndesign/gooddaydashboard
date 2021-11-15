340b1f8e9cd703d9f2da5cdcebe3495d023a1260
akndesign <alexanderkoziol@hotmail.com>
2021-11-15 00:57:25 -0500

    IT FINALLY WORKS ON HERUKO (LOCALLY)
    Icons are missing, but finalising. Also asteroid data is pulling everything.

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const request = require('request');
 
-app.get('/', function (req, res) {
-    //res.sendFile(path.join(__dirname, '/'));
-    res.sendFile(path.join(__dirname, '/'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 
+app.get('/nasa-asteriods/', (req, res) => {
+  request(
+    {url: "https://ssd-api.jpl.nasa.gov/fireball.api?"},
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+
+/*app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(express.static(__dirname + "/public/img"));
+*/
 app.listen(process.env.PORT || 5000, function () {
     console.log('Node app is working!');
 });
@@ -42,18 +73,7 @@ app.use((req, res, next) => {
 
 //console.log(nasaAPIDay, nasaAPIURL);
 
-app.get('/nasa-asteriods/', (req, res, next) => {
-  request(
-    {url: "https://ssd-api.jpl.nasa.gov/fireball.api"},
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err.message });
-      }
 
-      res.json(JSON.parse(body));
-    }
-  )
-});
 
 const PORT = process.env.PORT || 5000;
 express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
